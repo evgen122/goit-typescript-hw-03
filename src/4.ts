e2bd23b0bafbc8c85b15cafacc86ef4331a88c45
evgen122 <evg.khristenko@gmail.com>
2023-11-29 14:45:03 +0200

    22222

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,55 @@
+/** @format */
+
+class Key {
+  private signature: number;
+  constructor(signature: number = Math.random()) {
+    // this.signature = Math.random();
+  }
+
+  getSignature(this: Key) {
+    console.log(this.signature);
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {}
+
+  getKey() {
+    console.log(this.key);
+    return this.key;
+  }
+}
+
+abstract class House {
+  door: boolean;
+  key: Key;
+  tenants: Person[] = [];
+
+  comeIn(tenant: Person): void {
+    if (this.door) {
+      this.tenants.push(tenant);
+    }
+  }
+  abstract OpenDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  key: Key;
+  OpenDoor(key: Key): void {
+    if (this.key.getSignature() === key.getSignature()) {
+      this.door = true;
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
 const person = new Person(key);
 
-house.openDoor(person.getKey());
+house.OpenDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
