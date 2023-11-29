d8299a92f054d99f382a5351b04807aff3a466bc
evgen122 <evg.khristenko@gmail.com>
2023-11-29 14:55:34 +0200

    Update 1.ts

--- src/1.ts
+++ src/1.ts
@@ -6,18 +6,6 @@
  * @format
  */
 
-// class Student {
-//   public name: string;
-//   public age: number;
-//   public grade: string;
-
-//   constructor(name: string, age: number, grade: string) {
-//     this.name = name;
-//     this.age = age;
-//     this.grade = grade;
-//   }
-// }
-
 class Student {
   constructor(public name: string, public age: number, public grade: string) {}
 }
